e4dd0a6e042ea5c2ca757e5bcbff9016da690d7c
kannanpasupathy <44133823+kannanpasupathy@users.noreply.github.com>
2018-10-16 13:51:24 +1100

    hello to demo
    hello to demo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo"]}
 
 app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write("Hello world");
+    res.write("Jenkins - Github Demo");
     res.end();
 });
 
